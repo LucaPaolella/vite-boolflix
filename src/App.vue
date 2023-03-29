5ddf8d07dc86e1e320979d161ee7352b7f1da97d
Your Name <you@example.com>
2023-03-29 16:35:54 +0200

    milestone 0

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { store } from './store';
+
 
 </script>
 
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    characters: [],
+    charactersFound: 0,
+    searchKey: '',
+    searchStatus: ''
+})
