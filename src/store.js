0f82b4dc5146a2c153bc134dd1d88d21f34793a8
Your Name <you@example.com>
2023-03-30 16:58:05 +0200

    aggiornamento readme

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { reactive } from "vue";
 
 export const store = reactive({
     config: {
-        urlMovie: 'https://api.themoviedb.org/3/movie',
+        urlMovie: 'https://api.themoviedb.org/3/search/movie',
         apiKey: 'f18ce2abcd2d9bd622cfeffbe75a5d26',
         defaultLang: 'it-IT',
     },
