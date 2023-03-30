58e97d0c61da34860bac11404f46858531ebf51a
Your Name <you@example.com>
2023-03-30 18:05:41 +0200

    bandierine:tutte francesi

--- src/components/cardApp.vue
+++ src/components/cardApp.vue
@@ -1,6 +1,11 @@
 <script>
+import CountryFlag from "vue-country-flag-next"
+
 export default {
     name: "Card",
+    components: {
+        CountryFlag
+    },
     props: {
         info: Object
     }
@@ -13,6 +18,7 @@ export default {
         <h4>{{ info.original_title }}</h4>
         <div>{{ info.original_language }}</div>
         <div>{{ info.vote_average }}</div>
+        <country-flag country="fr" size="small" />
     </article>
 </template>
 
