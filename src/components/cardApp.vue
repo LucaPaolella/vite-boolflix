12f3804b2e4fd8a7576baf7c5d7d430bc7706556
Your Name <you@example.com>
2023-03-30 18:24:23 +0200

    bandierine: sistemate

--- src/components/cardApp.vue
+++ src/components/cardApp.vue
@@ -8,6 +8,19 @@ export default {
     },
     props: {
         info: Object
+    },
+    computed: {
+        getLanguage() {
+            switch (this.info.original_language) {
+                case "en":
+                    return "gb";
+                case "he":
+                    return "il";
+
+                default:
+                    return this.info.original_language;
+            }
+        }
     }
 }
 </script>
@@ -16,9 +29,10 @@ export default {
     <article>
         <h3>{{ info.title }}</h3>
         <h4>{{ info.original_title }}</h4>
-        <div>{{ info.original_language }}</div>
+        <div>{{ getLanguage }}</div>
+        <country-flag :country="getLanguage" size="small" />
         <div>{{ info.vote_average }}</div>
-        <country-flag country="fr" size="small" />
+
     </article>
 </template>
 
